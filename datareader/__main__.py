8c7443a2818008d7bbdeebf044c1d50172db449e
ga74kud <michaelhartmann10@googlemail.com>
2021-01-18 12:01:06 +0100

    dataset reader stanford
    Signed-off-by: ga74kud <michaelhartmann10@googlemail.com>

---
+++ datareader/__main__.py
@@ -0,0 +1,24 @@
+from source.preprocessing.dataprocessing import *
+import argparse
+import definitions
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--window_x', '-wix', type=int, help='windowsize in x-direction for savgol_filter', default=101,
+                        required=False)
+    parser.add_argument('--window_y', '-wiy', type=int, help='windowsize in y-direction for savgol_filter', default=101,
+                        required=False)
+    parser.add_argument('--poly_x', '-pox', type=int, help='polygon order in x-direction for savgol_filter', default=2,
+                        required=False)
+    parser.add_argument('--poly_y', '-poy', type=int, help='polygon order in y-direction for savgol_filter', default=2,
+                        required=False)
+    args = parser.parse_args()
+    params = vars(args)
+    params['PROJECT_ROOT'] = definitions.get_project_root()
+    obj=dataprocessing(params)
+    a=obj.dataset
+    None
+
+if __name__ == '__main__':
+    main()
