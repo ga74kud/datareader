aef9f7b0f05519a596c8546ac4bf2b3b5352f76c
Michael Hartmann <michaelhartmann10@googlemail.com>
2021-02-16 12:23:41 +0100

    e-step continued
    Signed-off-by: Michael Hartmann <michaelhartmann10@googlemail.com>

--- standalone/my_parameter_estimation.py
+++ standalone/my_parameter_estimation.py
@@ -6,13 +6,27 @@ Springer 2006"
 
 import numpy as np
 import scipy.stats as sd
+
+'''
+    E-Step Evaluate the responsibilites using the current parameter values
+'''
+def e_step(params, model, dataset):
+    gamma= [ [ 0 for i in range(params['K']) ] for j in range(params['N']) ]
+    for idx, act_point in enumerate(dataset):
+        modes_prob=[model['pi'][wlt]*sd.norm.pdf(act_point, model['mu'][wlt], model['Sigma'][wlt]) for wlt in range(0, params['K'])]
+        act_sum=np.sum(modes_prob)
+        for wlt in range(0, params['K']):
+            gamma[idx][wlt]=modes_prob[wlt]/act_sum
+    return gamma
+
+
 '''
     Evaluate the log likelihood
 '''
-def evaluate_loglikelihood(model, dataset):
+def evaluate_loglikelihood(params, model, dataset):
     erg=0
     for act_point in dataset:
-        modes_prob=[model['pi'][wlt]*sd.norm.pdf(act_point, model['mu'][wlt], model['Sigma'][wlt]) for wlt in range(0, len(model['pi']))]
+        modes_prob=[model['pi'][wlt]*sd.norm.pdf(act_point, model['mu'][wlt], model['Sigma'][wlt]) for wlt in range(0, params['K'])]
         act_sum=np.sum(modes_prob)
         erg+=np.log(act_sum)
     return erg
@@ -24,16 +38,20 @@ def evaluate_loglikelihood(model, dataset):
 N=100
 model = {'pi': [.4, .6], 'mu': [3, 2], 'Sigma': [0.1, .3]}
 my_model = {'pi': [.1, .9], 'mu': [0, 1], 'Sigma': [0.1, .3]}
-
+params={'N': 100, 'K': len(model['pi'])}
 ###############
 ### Dataset ###
 ###############
-modes_X=[model['pi'][wlt]*np.random.normal(model['mu'][wlt], model['Sigma'][wlt], N) for wlt in range(0, len(model['pi']))]
+modes_X=[model['pi'][wlt]*np.random.normal(model['mu'][wlt], model['Sigma'][wlt], N) for wlt in range(0, params['K'])]
 X=modes_X[0]
 for i in range(1, len(modes_X)):
     X+=modes_X[i]
-##############
-### Step 1 ###
-##############
-act_log_likeli=evaluate_loglikelihood(model, X)
+##########################################################
+### Step 1: Evaluate the log likelihood the first time ###
+##########################################################
+act_log_likeli=evaluate_loglikelihood(params, model, X)
+#####################################################
+### Step 2: E-Step, evaluate the responsibilities ###
+#####################################################
+gamma=e_step(params, model, X)
 None
