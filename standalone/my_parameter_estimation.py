f840adc4692275b1602262650792c9ce0dd3049c
Michael Hartmann <michaelhartmann10@googlemail.com>
2021-02-16 13:08:47 +0100

    parameter estimation
    Signed-off-by: Michael Hartmann <michaelhartmann10@googlemail.com>

--- standalone/my_parameter_estimation.py
+++ standalone/my_parameter_estimation.py
@@ -8,6 +8,53 @@ import numpy as np
 import scipy.stats as sd
 
 '''
+    one_cycle
+'''
+def one_cycle(params, my_model, X):
+    #####################################################
+    ### Step 2: E-Step, evaluate the responsibilities ###
+    #####################################################
+    gamma = e_step(params, my_model, X)
+    ##################################################
+    ### Step 3: M-Step, re-estimate the parameters ###
+    ##################################################
+    my_model = m_step(params, X, gamma)
+    ###########################################
+    ### Step 4: Evaluate the log likelihood ###
+    ###########################################
+    act_log_likeli = evaluate_loglikelihood(params, my_model, X)
+    return my_model, act_log_likeli
+'''
+    Get new sigmas (variances)
+'''
+def get_sigmas(params, dataset, new_mus, Nk, gamma):
+    new_sigmas = [0 for i in range(params['K'])]
+    for wlt in range(0, params['K']):
+        for idx, act_point in enumerate(dataset):
+            new_sigmas[wlt] += gamma[idx][wlt] * (act_point-new_mus[wlt])*np.transpose(act_point-new_mus[wlt])
+    new_sigmas = [new_sigmas[i] / Nk[i] for i in range(params['K'])]
+    return new_sigmas
+'''
+    Get new mean values
+'''
+def get_mu(params, dataset, gamma, Nk):
+    new_mu = [0 for i in range(params['K'])]
+    for wlt in range(0, params['K']):
+        for idx, act_point in enumerate(dataset):
+            new_mu[wlt] += gamma[idx][wlt] * act_point
+    new_mu = [new_mu[i] / Nk[i] for i in range(params['K'])]
+    return new_mu
+'''
+    M-Step, re-estimate the parameters
+'''
+def m_step(params, dataset, gamma):
+    Nk=np.sum(gamma, axis=0)
+    new_mus=get_mu(params, dataset, gamma, Nk)
+    new_sigmas = get_sigmas(params, dataset, new_mus, Nk, gamma)
+    new_pi=[Nk[i] / params['N'] for i in range(params['K'])]
+    return {'pi': new_pi, 'mu': new_mus, 'Sigma': new_sigmas}
+
+'''
     E-Step Evaluate the responsibilites using the current parameter values
 '''
 def e_step(params, model, dataset):
@@ -37,7 +84,7 @@ def evaluate_loglikelihood(params, model, dataset):
 ##################
 N=100
 model = {'pi': [.4, .6], 'mu': [3, 2], 'Sigma': [0.1, .3]}
-my_model = {'pi': [.1, .9], 'mu': [0, 1], 'Sigma': [0.1, .3]}
+my_model = {'pi': [.3, .7], 'mu': [3.2, 2.2], 'Sigma': [0.1, .3]}
 params={'N': 100, 'K': len(model['pi'])}
 ###############
 ### Dataset ###
@@ -49,9 +96,10 @@ for i in range(1, len(modes_X)):
 ##########################################################
 ### Step 1: Evaluate the log likelihood the first time ###
 ##########################################################
-act_log_likeli=evaluate_loglikelihood(params, model, X)
-#####################################################
-### Step 2: E-Step, evaluate the responsibilities ###
-#####################################################
-gamma=e_step(params, model, X)
-None
+act_log_likeli=evaluate_loglikelihood(params, my_model, X)
+###################################
+### Cycles: Iterative steps 2-4 ###
+###################################
+for cycle in range(0, 3):
+    my_model, act_log_likeli=one_cycle(params, my_model, X)
+    print(my_model)
