3a55222cc54fd84d7f425f79120e5e7f75be1d68
Michael Hartmann <michael.hartmann@v2c2.at>
2021-03-08 16:36:45 +0100

    gaussian mixture
    Signed-off-by: Michael Hartmann <michael.hartmann@v2c2.at>

--- standalone/predictive_velocities_v4.py
+++ standalone/predictive_velocities_v4.py
@@ -1,4 +1,4 @@
-import warnings
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy.stats import multivariate_normal
@@ -38,35 +38,53 @@ class causal_prob(object):
             second_mat=np.matmul(a, cov_hf)
             erg[wlt]=cov_ff-second_mat
         return erg
-    def conditioned_pi(self, mu, Sigmas, x_h, pi):
-        erg={new_list: [] for new_list in range(len(Sigmas))}
-        vart = [multivariate_normal(mean=mu[qrt]["h"], cov=Sigmas[qrt]["hh"]).pdf(x_h) for qrt in range(0, len(erg))]
-        for wlt in erg:        # test=var.pdf([0, 0])
-            erg[wlt]=vart[wlt]/np.sum(vart)
+    def conditioned_pi(self, vel, x_h):
+        all_val=[]
+        for wlt in vel:
+            mu_h, mu_f, cov_hh, cov_fh, cov_hf, cov_ff = self.get_small_pieces(wlt)
+            all_val.append(vel[wlt]['pi']*multivariate_normal(mean=mu_h, cov=cov_hh).pdf(x_h))
+        erg = {nl: all_val[nl]/np.sum(all_val) for nl in range(0, len(self.mean))}
         return erg
     def get_dataset(self, start_pos,vel):
 
         xs, ys = np.random.multivariate_normal(start_pos['mu'], start_pos['Sigma'], 5000).T
-        idx=np.random.choice(2, 5000, p=[vel[rqt]['pi'] for rqt in vel])
+        idx=np.random.choice(len(vel), 5000, p=[vel[rqt]['pi'] for rqt in vel])
         xe, ye=np.zeros((len(xs),)), np.zeros((len(ys),))
         for ix, qrt in enumerate(idx):
             vx, vy = np.random.multivariate_normal(vel[qrt]['mu'], vel[qrt]['Sigma'])
             xe[ix], ye[ix]=xs[ix]+vx, ys[ix]+vy
         d = {'idx': idx, 'xs': xs, 'ys': ys, 'xe': xe, 'ye': ye}
         self.df = pd.DataFrame(data=d)
-        sub_df=[self.df.loc[self.df['idx'] == qrt].drop('idx', axis=1) for qrt in range(0, len(vel))]
-        self.mean=[np.array(sub_df[qrt].mean()) for qrt in range(0, len(sub_df))]
-        self.cov=[np.array(sub_df[qrt].cov()) for qrt in range(0, len(sub_df))]
+        self.sub_df=[self.df.loc[self.df['idx'] == qrt].drop('idx', axis=1) for qrt in range(0, len(vel))]
+        self.mean=[np.array(self.sub_df[qrt].mean()) for qrt in range(0, len(self.sub_df))]
+        self.cov=[np.array(self.sub_df[qrt].cov()) for qrt in range(0, len(self.sub_df))]
+        self.pi=[vel[qrt]['pi'] for qrt in vel]
+    def plot_the_scene(self):
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        ax.scatter(self.df['xs'], self.df['ys'], label='initial position', color='black')
+        sel_col=['blue', 'green', 'red', 'cyan', 'orange', 'yellow']
+        [ax.scatter(qrt['xe'], qrt['ye'], label=str(idx), color=sel_col[idx]) for idx, qrt in enumerate(self.sub_df)]
+        ax.legend()
+        plt.grid()
+        plt.show()
 
 
 
-start_pos={'mu': np.array([1, 2]), 'Sigma': np.array([[1, 0.3], [0.3, 1]])}
-vel={0: {'pi':.6, 'mu': np.array([4, 4]), 'Sigma': np.array([[1, 0.5], [0.5, 1]])},
-     1:{'pi':.4, 'mu': np.array([-4, 2]), 'Sigma': np.array([[1, 0.2], [0.2, 1]])}}
+start_pos={'mu': np.array([1, 2]), 'Sigma': np.array([[.4, 0], [0, .4]])}
+vel={0: {'pi':.5, 'mu': np.array([8, 8]), 'Sigma': np.array([[1, 0.5], [0.5, 1]])},
+     1:{'pi':.3, 'mu': np.array([-8, 2]), 'Sigma': np.array([[.4, -0.6], [-0.6, .4]])},
+     2: {'pi': .2, 'mu': np.array([1, -7]), 'Sigma': np.array([[.4, -0.6], [-0.6, .4]])}
+     }
 
+
+xh=np.array([4.5, 4.5])
 obj_causal = causal_prob()
 obj_causal.get_dataset(start_pos,vel)
-new_mu=obj_causal.conditioned_mu(np.array([5, 8]))
+new_mu=obj_causal.conditioned_mu(xh)
 print(new_mu)
 new_Sigma=obj_causal.conditioned_Sigma()
 print(new_Sigma)
+new_pi=obj_causal.conditioned_pi(vel, xh)
+print(new_pi)
+obj_causal.plot_the_scene()
